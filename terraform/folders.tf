d011f501f5253946aa473efae0abaad6010440e6
murakami.kyoya <murakami.kyoya@moneyforward.co.jp>
2024-12-16 12:01:42 +0900

    Update

--- terraform/folders.tf
+++ terraform/folders.tf
@@ -1,5 +1,5 @@
 
-#resource "jenkins_folder" "ca" {
-#  name        = "ca"
-#  description = "This is the folder for the CA jobs"
-#}
+resource "jenkins_folder" "ca" {
+  name        = "ca"
+  description = "This is the folder for the CA jobs"
+}
--- terraform/jobs.tf
+++ terraform/jobs.tf
@@ -4,11 +4,11 @@ locals {
   ca_jobs = {for job in local.raw_ca_jobs.jobs : job.name => job.description}
 }
 
-#module "ca_jobs" {
-#  source          = "./modules/"
-#  for_each        = local.ca_jobs
-#  job_name        = each.key
-#  folder          = jenkins_folder.ca.name
-#  job_description = each.value
-#  branch          = var.branch
-#}
+module "ca_jobs" {
+  source          = "./modules/"
+  for_each        = local.ca_jobs
+  job_name        = each.key
+  folder          = jenkins_folder.ca.name
+  job_description = each.value
+  branch          = var.branch
+}
